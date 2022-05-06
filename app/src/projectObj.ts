1d695da43cece59a6925c44768c94dbcfa784a22
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2022-05-06 18:18:59 +0200

    working

--- app/src/projectObj.ts
+++ app/src/projectObj.ts
@@ -1,7 +1,39 @@
-dependencyImports
+export function projectObj(source: object, project: object) {
+  if (project === undefined) return source
+  const keys = Object.keys(project)
+  if (keys.length === 0) return source
 
-export function projectObj() {
-  
+
+  const whiteListKeys = []
+  const blackListKeys = []
+  for (const key of keys) {
+    if (project[key]) whiteListKeys.push(key)
+    else blackListKeys.push(key)
+  }
+
+  const whiteListeMode = whiteListKeys.length !== 0
+  const projectFunc = whiteListeMode ? projectObjectWhiteList : projectObjectBlackList
+  const relevantProjectionKeys = whiteListeMode ? whiteListKeys : blackListKeys
+
+  return source instanceof Array ? source.map((ob) => projectFunc(ob, project, relevantProjectionKeys)) : projectFunc(source, project, relevantProjectionKeys)
+}
+
+function projectObjectWhiteList(source: object, project: object, whiteList: string[]) {
+  const projection = {}
+  for (const key of whiteList) {
+    if (source[key] !== undefined) projection[key] = source[key] instanceof Object ? projectObj(source[key], project[key]) : source[key]
+  }
+  return projection
+}
+function projectObjectBlackList(source: object, project: object, blackList: string[]) {
+  const projection = { ...source }
+  for (const key of blackList) {
+    if (source[key] !== undefined) delete projection[key]
+  }
+  for (const key in projection) {
+    if (projection[key] instanceof Object) projection[key] = projectObj(source[key], project[key])
+  }
+  return projection
 }
 
 export default projectObj
--- repl/src/repl.ts
+++ repl/src/repl.ts
@@ -1,4 +1,27 @@
 import projectObj from "../../app/src/projectObj"
 //const testElem = document.querySelector("#test")
 
-projectObj()
+
+const marc = {
+  name: "Marc",
+  age: 22,
+  dob: new Date(1980, 1, 10).toLocaleDateString(),
+}
+
+const max = {
+  name: "Max",
+  age: 20,
+  dob: new Date(1980, 1, 20).toLocaleDateString(),
+  friend: marc
+}
+
+// @ts-ignore
+marc.friend = max
+
+
+const project = {
+  dob: false,
+  friend: {}
+}
+
+console.log(projectObj(max, project))
